a7b14ad575cb8906e81e08b95be3f29fc9909adf
seanwestfall <sean.westfall@gmail.com>
2017-11-01 00:49:45 -0700

    Update to the README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# spring-thymeleaf-example
+# spring + thymeleaf microservice example
+
+## Implementation of an Aggregator Microservice Design Pattern
 
 ### Production App: https://cryptic-sierra-23488.herokuapp.com/
--- src/main/java/com/example/myapp/DemoApplication.java
+++ src/main/java/com/example/myapp/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.myapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.ui.Model;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
@@ -15,7 +16,14 @@ public class DemoApplication {
       return "Hello World!";
     }
 
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
 }
